869d30136520b8d1a55e112fee8d16d3a353a10c
jwtavare <jwtavare@gmail.com>
2021-12-26 15:28:15 -0500

    Arrays

---
+++ javatesting1/javatraining/src/LearningArrays.java
@@ -0,0 +1,14 @@
+public class LearningArrays {
+
+    public static void main(String[] args) {
+       double [] values = new double[100];
+       values [0] = 1000.00;
+       values [99] = 93432.01;
+       System.out.println(values[0]);
+
+        
+    }
+    
+} 
+    
+
