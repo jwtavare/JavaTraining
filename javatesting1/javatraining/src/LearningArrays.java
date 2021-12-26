4a348ba3ea43074c2975615ae7cceca1a89c3bea
jwtavare <jwtavare@gmail.com>
2021-12-26 15:36:19 -0500

    Strings

--- javatesting1/javatraining/src/LearningArrays.java
+++ javatesting1/javatraining/src/LearningArrays.java
@@ -6,6 +6,11 @@ public class LearningArrays {
        values [99] = 93432.01;
        System.out.println(values[0]);
 
+       String [] words = new String[3];
+       words[0] = "My";
+       words[1] = "name";
+       words[2] = "is"; 
+       System.out.println(words[0]);
         
     }
     
