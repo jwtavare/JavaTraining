a2744f8f17cda499383d6438c760f5b8eb157775
jwtavare <jwtavare@gmail.com>
2021-12-27 13:26:45 -0500

    Latest

--- javatesting1/javatraining/src/LearningMethods.java
+++ javatesting1/javatraining/src/LearningMethods.java
@@ -3,6 +3,7 @@ public class LearningMethods {
     public static void main(String[] args) {
         System.out.println("Print initial stuff");
         MyUtils.printSomeStuff(55);
+        MyUtils.sum2Numbers(1012, 1123);
     }
 }
 
--- javatesting1/javatraining/src/MyUtils.java
+++ javatesting1/javatraining/src/MyUtils.java
@@ -6,4 +6,7 @@ public class MyUtils{
     public static void printSomeStuff(Integer arg){
         System.out.println(arg);
     }
+    public static void sum2Numbers(Integer firstArg, Integer secArg) {
+        System.out.println(firstArg + secArg);
+    }
 }
