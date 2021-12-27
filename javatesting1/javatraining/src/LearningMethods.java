0d2ed919ab0a6658297164d474da57cdd7a46f3d
jwtavare <jwtavare@gmail.com>
2021-12-27 11:54:45 -0500

    Methods and Class

---
+++ javatesting1/javatraining/src/LearningMethods.java
@@ -0,0 +1,8 @@
+public class LearningMethods {
+    
+    public static void main(String[] args) {
+        System.out.println("Print initial stuff");
+        MyUtils.printSomeStuff(55);
+    }
+}
+
---
+++ javatesting1/javatraining/src/MyUtils.java
@@ -0,0 +1,9 @@
+public class MyUtils{
+    
+    public static void printSomeStuff(String arg){
+        System.out.println(arg);
+    }
+    public static void printSomeStuff(Integer arg){
+        System.out.println(arg);
+    }
+}
