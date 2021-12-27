77472ec0b623c50143fad4f5c2fc3383f3020f5d
jwtavare <jwtavare@gmail.com>
2021-12-27 10:16:14 -0500

    ControlFlow

---
+++ javatesting1/javatraining/src/ControlFlow.java
@@ -0,0 +1,17 @@
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+public class ControlFlow {
+    
+    public static void main(String[] args) {
+
+        boolean = hungry;
+
+        if (hungry){
+            System.out.println("I am starving ...");
+        } else
+            System.out.println("I'm not hungry ...");
+        {
+
+        }
+    }
+}
