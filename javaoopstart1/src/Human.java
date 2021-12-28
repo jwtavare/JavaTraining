f54a76dd51a8a042b181e45ff145d56ba620601f
jwtavare <jwtavare@gmail.com>
2021-12-28 16:53:18 -0500

    Latest

--- javaoopstart1/src/Human.java
+++ javaoopstart1/src/Human.java
@@ -7,6 +7,7 @@ public class Human {
     int heightInInches;
     String eyeColor;
 
+    //constructor with this keyword
     public Human(String name, int age, int heightInInches, String eyeColor) {
         this.name = name;
         this.age = age;
@@ -15,7 +16,7 @@ public class Human {
     }
     
     public void speak(){
-        System.out.println("Hello my name is " + name + System.lineSeparator());
+        System.out.println("Hello my name is " + name);
         System.out.println("My height in inches is " + heightInInches);
         System.out.println("My age is " + age);
         System.out.println("My eyes are " + eyeColor + System.lineSeparator());
