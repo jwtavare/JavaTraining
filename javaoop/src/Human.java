a6b6623fec5b6f553c435afce98a684343c0d4c7
jwtavare <jwtavare@gmail.com>
2021-12-28 14:31:29 -0500

    human 1

---
+++ javaoop/src/Human.java
@@ -0,0 +1,26 @@
+public class Human {
+
+    String name;
+    int age;
+    int heightInInches;
+    String eyeColor;
+
+    public void speak(){
+        System.out.println("Hello my name is " + name);
+        System.out.println("My height in inches is " + heightInInches);
+        System.out.println("My age is " + age);
+        System.out.println("My eyes are " + eyeColor);
+    }
+
+    public void eat(){
+        System.out.println("eating....");
+    }
+
+    public void walk(){
+        System.out.println("walking...");
+    }
+
+    public void work(){
+        System.out.println("working...");
+    }
+}
