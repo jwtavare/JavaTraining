6f73a02ba5fb6fefef236de6a9170c10bb377785
jwtavare <jwtavare@gmail.com>
2021-12-28 10:42:02 -0500

    Latest2

--- javatesting1/javatraining/src/MyUtils.java
+++ javatesting1/javatraining/src/MyUtils.java
@@ -3,9 +3,9 @@ public class MyUtils{
     public static String printSomeStuff(String arg){
         return arg;
     }
-    // public static void printSomeStuff(Integer arg){
-        // System.out.println(arg);
-    // }
+    public static void printSomeStuff(Integer arg){
+        System.out.println(arg);
+    }
     public static void sum2Numbers(Integer firstArg, Integer secArg) {
         System.out.println(firstArg + secArg);
     }
