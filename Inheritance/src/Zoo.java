62522ed5809421e3ccb1d74390825298fe41ad38
jwtavare <jwtavare@gmail.com>
2021-12-30 16:13:55 -0500

    Latest with new Inherit project

---
+++ Inheritance/src/Zoo.java
@@ -0,0 +1,23 @@
+public class Zoo {
+    
+    public static void main(String [] args) {
+         
+        Animal animal1 = new Animal("Fox",20,35,20);
+        //Instance Variable of type Animal is created in memory
+        //Somewhere else in memory the object is created
+        //Variable for animal1 will point to the memory address for the Animal object
+        Animal animal2 = new Animal("Cow",100,100,25);
+        Animal animal3 = new Animal("Wolf",25,25,100);
+        Bird bird1 = new Bird(); 
+        Fish fish1 = new Fish();
+        //Main class initializing objects by referencing constructors in other classes
+
+        animal1.eat();
+        //Variable calls associated object in memory and tells it to execute eat() Method
+        animal2.eat();
+        animal3.eat();
+        bird1.fly();
+        fish1.swim();
+
+    }
+}
