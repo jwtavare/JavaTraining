1af60ba036f1ea41c28d992e2cd955198f549027
jwtavare <jwtavare@gmail.com>
2022-01-03 12:42:25 -0500

    Move

--- AbstractClasses/src/Zoo.java
+++ AbstractClasses/src/Zoo.java
@@ -5,5 +5,9 @@ public class Zoo {
         sparrow1.move();
         Chicken chicken1 = new Chicken("White", 10, 5, 15);
         chicken1.eat();
+        Animal sparrow2 = new Sparrow("Green", 5, 10, 20);
+        sparrow2.swim();
+        Animal fish1 = new Fish("Perch", 2, 5, 3);
+        fish1.move();
     }
 }
