71ea468659ff00e93d8115930af63da3bd8b3ce0
jwtavare <jwtavare@gmail.com>
2022-01-03 10:12:22 -0500

    Latest

--- AbstractClasses/src/Zoo.java
+++ AbstractClasses/src/Zoo.java
@@ -3,5 +3,7 @@ public class Zoo {
     public static void main(String [] args) {
         Sparrow sparrow1 = new Sparrow("Blue", 15, 5, 1);
         sparrow1.move();
+        Chicken chicken1 = new Chicken("White", 10, 5, 15);
+        chicken1.eat();
     }
 }
