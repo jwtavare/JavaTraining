12d06e9468b75e33e8b0c379fe4a1631ea24ec82
jwtavare <jwtavare@gmail.com>
2022-01-30 14:53:02 -0500

    Not it!!!

--- StringsPractice/src/StringsPractice.java
+++ StringsPractice/src/StringsPractice.java
@@ -2,8 +2,17 @@ public class StringsPractice {
     public static void main(String[] args) {
         
         String str = "ABCDEF";
-        String extractedString = str.substring (0);
+        String extractedString = str.substring (0,3);
 
         System.out.println(extractedString);
+
+        String a = "Hello";
+        String b = "there";
+
+        if(a.equals("Hello")){
+            System.out.println(a);
+        } else {
+            System.out.println("Not it!");
+        }
     }
 }
