126d06b69e1f0ae33d4b04cb90900c9484321595
jwtavare <jwtavare@gmail.com>
2022-02-13 10:16:10 -0500

    latest

--- StringsPractice/src/LoopsPractice.java
+++ StringsPractice/src/LoopsPractice.java
@@ -1,8 +1,13 @@
 public class LoopsPractice {
 
     public static void main(String[] args) {
-    
+        
+        String str = "We have a large inventory of things in our warehouse" 
+        + " the category:apperal and the slightly"
+        + "more in demand category:makeup and the category:furniture and...";
         int count = 0;
+
+        printCategories(str);
 
         while(count<=100){
             System.out.println("JT:" + count);
@@ -13,5 +18,24 @@ public class LoopsPractice {
             }
         }
     }
-}
+    //** extract all categories from the string arg
+    public static void printCategories(String string) {
+        int i = 0;
+        while(true){
+            int found = string.indexOf("category:", i);
+            //variable returning first index locator whenever "category" is detected
+            
+            if (i == -1) break;
+            int start = found + 9; 
+            //accommodates starting position 9 chars from start 
+            //of categories word plus colon
+            
+            int end = string.indexOf(" ", start); 
+            System.out.println(string.substring(start, end));
+            i=end+1;
+            }
+             
+        }
+    }
+
 
