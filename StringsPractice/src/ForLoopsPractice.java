b663e04ba2a8cd6a9944543f476692552980100d
jwtavare <jwtavare@gmail.com>
2022-02-21 09:40:43 -0500

    last

--- StringsPractice/src/ForLoopsPractice.java
+++ StringsPractice/src/ForLoopsPractice.java
@@ -4,8 +4,10 @@ public class ForLoopsPractice {
         //use for loops for specific cases to increment a variable
         //also use when you know exact times you want to look through block of code
         
+        String name = "JasonTavares 143 Prospect Street";
+
         for (int i=0; i<100; i++){
-            System.out.println(i);
+            System.out.println("char: " + name.charAt(i));
         }
     }
 }
