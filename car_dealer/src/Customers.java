9ce061922990f994f6cc669bc25d42e79cdbecab
jwtavare <jwtavare@gmail.com>
2022-02-22 19:23:18 -0500

    Last

---
+++ car_dealer/src/Customers.java
@@ -0,0 +1,13 @@
+public class Customers {
+    
+    String name;
+    int age;
+    String address;
+
+    public Customers(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+}
