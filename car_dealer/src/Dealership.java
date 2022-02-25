71ad6040cd0f7778a65a5126e80201cc28e021a1
jwtavare <jwtavare@gmail.com>
2022-02-25 18:15:34 -0500

    Last

--- car_dealer/src/Dealership.java
+++ car_dealer/src/Dealership.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
-import javax.swing.text.Position;
-
 public class Dealership {
     public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What type of car are you looking for? ");
+        String carType = scan.nextLine();
+        System.out.println("What year of car? ");
+        String carYear = scan.nextLine();
+        System.out.println("What color car would you like? ");
+        String carColor = scan.nextLine();
+
+        Vehicles CarInfo = new Vehicles(carType, carYear, carColor);
         
-        Scanner name = new Scanner(System.in);
-        Scanner age = new Scanner(System.in);
-        Scanner position = new Scanner (System.in);
-        
-        Employees employeeInfo = new Employees(name, age, position );
+
 
     }
 }
