95c09510058ebd4cb78a6bd32f6b72ff289bee6b
jwtavare <jwtavare@gmail.com>
2022-02-26 16:23:37 -0500

    Last

--- ReadFile/src/readFile.java
+++ ReadFile/src/readFile.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class readFile {
     public static void main(String[] args) throws Exception {
@@ -12,10 +16,28 @@ public class readFile {
                     System.out.println(data);
                 }
                 myReader.close();
-            } catch (FileNotFoundException e) {
+            } 
+            
+            catch (IOException e) {
                 //TODO: handle exception
-                System.out.println("An Exception has occurred ");
                 e.printStackTrace();
             }
-    } 
-}
+
+            String fileName = "/Users/tavares/Documents/File.rtf";
+            try 
+                (RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
+                
+                    raf.seek(12);
+                    System.out.println(raf.readLine());
+                }
+            catch (IOException e) {
+                //TODO: handle exception
+                System.out.println(e);
+            }    
+            for (int i = 1; i<=2 ; i++){
+            String line = Files.readAllLines(Paths.get("/Users/tavares/Documents/File.rtf")).get(i);
+            System.out.println(line);
+            }
+        }
+}    
+
